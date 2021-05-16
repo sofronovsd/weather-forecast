aafee3162b924fa535fe5c4683cec59e1acf0681
Sofronov Semyon <sofronov.semyon@gmail.com>
2021-05-16 17:20:45 +0400

    add main page background

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
+import './MainPage.scss'
+
 const MainPage = () => {
   return (
-    <>
+    <article className="main-page">
       <header>Weather forecast</header>
       <main></main>
       <footer>
         <span className="text text--small text--weak">C ЛЮБОВЬЮ ОТ MERCURY DEVELOPMENT</span>
       </footer>
-    </>
+    </article>
   )
 }
 
