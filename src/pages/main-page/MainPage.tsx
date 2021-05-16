21a35a228715557d2bdfb9263eabeb4d8448ecb1
Sofronov Semyon <sofronov.semyon@gmail.com>
2021-05-16 16:12:46 +0400

    add prettier and sass

---
+++ src/pages/main-page/MainPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MainPage = () => {
+  return (
+    <>
+      <header>Weather forecast</header>
+      <main></main>
+      <footer>C ЛЮБОВЬЮ ОТ MERCURY DEVELOPMENT</footer>
+    </>
+  )
+}
+
+export default MainPage
