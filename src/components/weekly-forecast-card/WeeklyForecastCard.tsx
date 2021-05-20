d7de24e84dc9cd355da57e3310daa51d2b0387ee
Sofronov Semyon <sofronov.semyon@gmail.com>
2021-05-20 13:01:49 +0400

    update weekly forecast card

--- src/components/weekly-forecast-card/WeeklyForecastCard.tsx
+++ src/components/weekly-forecast-card/WeeklyForecastCard.tsx
@@ -4,13 +4,20 @@ import Card from '../card/Card'
 import cities from './cities'
 import Dropdown from '../dropdown/Dropdown'
 
+import mockImage from '../../assets/Academy-Weather-bg160.svg'
 import './WeeklyForecastCard.scss'
 
 const WeeklyForecastCard = () => {
   return (
     <Card>
-      <h1>7 Days Forecast</h1>
+      <h1 className="weekly-forecast-card__header">7 Days Forecast</h1>
       <Dropdown className="weekly-forecast-card__dropdown" placeholder="Select city" options={cities} />
+      <div className="weekly-forecast-card__image-container">
+        <img src={mockImage} className="weekly-forecast-card__mock-image" alt="mocked weather" />
+        <span className="text text--secondary text--bold">
+          Fill in all the fields and the weather will be displayed
+        </span>
+      </div>
     </Card>
   )
 }
