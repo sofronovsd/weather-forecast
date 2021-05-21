1b75fac265aa2fb4497e2af872ad02f2f2bb23a2
Sofronov Semyon <sofronov.semyon@gmail.com>
2021-05-21 16:15:22 +0400

    add weekly forecast cards

--- src/components/weather-card/WeatherCard.tsx
+++ src/components/weather-card/WeatherCard.tsx
@@ -8,18 +8,20 @@ import moment from 'moment'
 
 type WeatherCardProps = {
   className?: string
+  iconClassName?: string
   icon: string
   temp: number
   date: number
 }
 
-const WeatherCard = ({ className, icon, temp, date }: WeatherCardProps) => {
+const WeatherCard = ({ className, icon, temp, date, iconClassName }: WeatherCardProps) => {
   const cardStyles = classNames(className, 'weather-card')
+  const iconStyles = classNames(iconClassName, 'weather-card__icon')
 
   const imgSrc = `http://openweathermap.org/img/wn/${icon}@2x.png`
   return (
     <Card className={cardStyles}>
-      <img className="weather-card__icon" src={imgSrc} alt="weather" />
+      <img className={iconStyles} src={imgSrc} alt="weather" />
       <h1 className="weather-card__temp text--weak">{`${temp > 0 ? '+' : '-'}${Math.floor(temp)}°`}</h1>
       <span className="weather-card__date text text--weak text--bold">{moment(date * 1000).format('DD MMM YYYY')}</span>
     </Card>
--- src/components/weekly-forecast-card/WeeklyForecastCard.tsx
+++ src/components/weekly-forecast-card/WeeklyForecastCard.tsx
@@ -1,12 +1,16 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 
 import Card from '../card/Card'
 import cities, { City } from '../../assets/cities'
 import Dropdown from '../dropdown/Dropdown'
 
 import mockImage from '../../assets/Academy-Weather-bg160.svg'
+import arrowLeft from '../../assets/chevron-left.svg'
+import arrowRight from '../../assets/chevron-right.svg'
 import './WeeklyForecastCard.scss'
 import { DailyForecast, getWeatherForecast } from '../../api/weather-api'
+import WeatherCard from '../weather-card/WeatherCard'
+import classNames from 'classnames'
 
 const initialCity: City = {
   name: '',
@@ -22,6 +26,15 @@ const WeeklyForecastCard = () => {
   const [city, setCity] = useState(initialCity)
   const [forecast, setForecast] = useState(initialForecast)
   const [loading, setLoading] = useState(false)
+  const [index, setIndex] = useState(0)
+
+  const leftArrowStyles = classNames('weekly-forecast-card__arrow', 'weekly-forecast-card__arrow--left', {
+    'weekly-forecast-card__arrow--disabled': index === 0
+  })
+
+  const rightArrowStyles = classNames('weekly-forecast-card__arrow', 'weekly-forecast-card__arrow--right', {
+    'weekly-forecast-card__arrow--disabled': index === 5
+  })
 
   const handleSelectCity = (city: City) => {
     setCity(city)
@@ -46,6 +59,23 @@ const WeeklyForecastCard = () => {
       console.log('forecast', forecast)
     }
   }, [forecast])
+
+  const isForecast = useMemo(() => {
+    return forecast.daily.length > 0
+  }, [forecast])
+
+  const handleLeftArrowClick = () => {
+    if (index > 0) {
+      setIndex(prev => prev - 1)
+    }
+  }
+
+  const handleRightArrowClick = () => {
+    if (index < 5) {
+      setIndex(prev => prev + 1)
+    }
+  }
+
   return (
     <Card>
       <h1 className="weekly-forecast-card__header">7 Days Forecast</h1>
@@ -57,10 +87,37 @@ const WeeklyForecastCard = () => {
         disabled={loading}
       />
       <div className="weekly-forecast-card__image-container">
-        <img src={mockImage} className="weekly-forecast-card__mock-image" alt="mocked weather" />
-        <span className="text text--secondary text--bold">
-          Fill in all the fields and the weather will be displayed
-        </span>
+        {isForecast ? (
+          <div className="weekly-forecast-card__forecast-container">
+            <img className={leftArrowStyles} src={arrowLeft} onClick={handleLeftArrowClick} alt="left" />
+            <img className={rightArrowStyles} src={arrowRight} onClick={handleRightArrowClick} alt="left" />
+            <WeatherCard
+              icon={forecast.daily[index].weather[0].icon}
+              temp={forecast.daily[index].temp.max}
+              date={forecast.daily[index].dt}
+              iconClassName="weekly-forecast-card__weather-icon"
+            />
+            <WeatherCard
+              icon={forecast.daily[index + 1].weather[0].icon}
+              temp={forecast.daily[index + 1].temp.max}
+              date={forecast.daily[index + 1].dt}
+              iconClassName="weekly-forecast-card__weather-icon"
+            />
+            <WeatherCard
+              icon={forecast.daily[index + 2].weather[0].icon}
+              temp={forecast.daily[index + 2].temp.max}
+              date={forecast.daily[index + 2].dt}
+              iconClassName="weekly-forecast-card__weather-icon"
+            />
+          </div>
+        ) : (
+          <>
+            <img src={mockImage} className="weekly-forecast-card__mock-image" alt="mocked weather" />
+            <span className="text text--secondary text--bold">
+              Fill in all the fields and the weather will be displayed
+            </span>
+          </>
+        )}
       </div>
     </Card>
   )
